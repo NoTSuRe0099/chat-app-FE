d530347f87be305e391dbbe346866375234a25f4
RPatil <Rpatil@texala.net>
2024-12-04 12:15:16 +0530

    fixed socket connection issue

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -7,7 +7,8 @@ import React, {
 } from 'react';
 import { useSelector } from 'react-redux';
 import { Socket, io } from 'socket.io-client';
-import { selectAuth } from '../auth/AuthSlice';
+import { AuthState, selectAuth } from '../auth/AuthSlice';
+import { store } from '../store';
 
 interface SocketContextProps {
   socket: Socket | null;
@@ -23,18 +24,24 @@ export const useSocket = () => {
 
 const SERVER_URL = import.meta.env.VITE_BASE_API_URL || 'http://localhost:5000';
 
-export const SocketProvider: React.FC<{ children: ReactNode }> = ({
+export const SocketProvider: React.FC<{ children: ReactNode; }> = ({
   children,
 }) => {
   const [socket, setSocket] = useState<Socket | null>(null);
-  const userState = useSelector(selectAuth);
+
+  const authState: AuthState = store?.getState()?.authState;
+  const userState: AuthState = useSelector(selectAuth);
 
   useEffect(() => {
-    if (userState?.isAuthenticated) {
+    if (userState?.isAuthenticated || authState?.isAuthenticated) {
+      const token = userState?.access_token || authState?.access_token || '';
       const _socket = io(SERVER_URL, {
         autoConnect: true,
         withCredentials: true,
         transports: ['websocket'], // Specify transports if needed
+        auth: {
+          access_token: token
+        }
       });
 
       _socket.on('connect', () => {
@@ -51,7 +58,7 @@ export const SocketProvider: React.FC<{ children: ReactNode }> = ({
         _socket.disconnect(); // Disconnect the socket when component unmounts
       };
     }
-  }, [userState?.isAuthenticated]);
+  }, [userState, authState]);
 
   return (
     <SocketContext.Provider value={{ socket }}>
